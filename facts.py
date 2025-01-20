15c7c5f56cfbcbf3fbae84d183ee1bc59fdf2e81
JosueBogran <46737143+JosueBogran@users.noreply.github.com>
2025-01-20 11:27:40 -0500

    Update facts.py

--- facts.py
+++ facts.py
@@ -12,6 +12,6 @@ a.Order_ID
 , (c.standard_price * ((100-discount_rate)/100)) * a.Quantity AS sales_amount
 , a.discount_rate AS discount_percentage
 FROM
-josuesunnydata.sweetcoffeetree.base AS a
+sweetcoffeetree.base AS a
 LEFT JOIN sweetcoffeetree.dim_locations AS b ON (a.Location_ID = b.record_id)
 LEFT JOIN sweetcoffeetree.dim_products AS c ON (a.Product_ID = c.product_id AND a.Order_Date BETWEEN c.from_date AND c.to_date)
